e6ab4cb4044424ad50ee4996596e55353eef89b6
Senka107 <93744556+Pier-Alexis@users.noreply.github.com>
2024-12-04 18:22:32 -0500

    Update ultimate_windows_manager.sh

--- ultimate_windows_manager.sh
+++ ultimate_windows_manager.sh
@@ -35,47 +35,33 @@ wine ~/.wine/drive_c/Program\\ Files/Epic\\ Games/Launcher/Portal/Binaries/Win64
 EOF
 
     chmod +x ~/proton_fortnite/launch_fortnite.sh
-    echo "=== Configuration terminée ! Utilisez ~/proton_fortnite/launch_fortnite.sh pour lancer Fortnite ==="
+    echo "=== Configuration finished ! Utilisez ~/proton_fortnite/launch_fortnite.sh pour lancer Fortnite ==="
 }
 
 function show_menu() {
     echo "Choose an option :"
-    echo "2. Launch a game or a Windows Application"
-    echo "3. Manage DLL and dependencies"
-    echo "4. Modify advanced configuration (DXVK, Wine)"
-    echo "6. Diagnostic an application or a game"
     echo "1. Setup Fortnite"
-    echo "7. Exit"
-}
-
-function setup_environment() {
-    echo "Configuration de l'environnement complet..."
-    ./install_dependencies.sh
-    echo "Environnement configuré avec succès !"
+    echo "2. Launch a game or a Windows Application"
+    echo "3. Diagnostic an application or a game"
+    echo "4. Exit"
 }
 
 function launch_application() {
-    read -p "Entrez le chemin complet du fichier .exe ou .msi : " file_path
-    if [[ "$file_path" == *.msi ]]; then
+    read -p "Type the full path of the file starting at your username in /home/ : " file_path
+    if [[ "home/$file_path" == *.msi ]]; then
         ./install_msi.sh "$file_path" /i
     elif [[ "$file_path" == *.exe ]]; then
         wine "$file_path"
     else
-        echo "Format de fichier non pris en charge : $file_path"
+        echo "Invalid .exe or .msi file : $file_path"
     fi
 }
 
-function configure_mime() {
-    echo "Configuration des associations MIME pour .exe et .msi..."
-    ./configure_mime.sh
-    echo "Associations MIME configurées avec succès !"
-}
-
 function diagnose_application() {
-    read -p "Entrez le chemin complet du fichier .exe ou .msi : " file_path
-    echo "Lancement du diagnostic pour $file_path..."
+    read -p "Type the full path of the .msi or .exe file : " file_path
+    echo "Starting the diagnostic for $file_path ..."
     WINEDEBUG=+all wine "$file_path" &> wine_diagnostic.log
-    echo "Diagnostic terminé. Rapport enregistré dans wine_diagnostic.log"
+    echo "Diagnostic finished. For further details go check the wine_diagnostic.log file created."
 }
 
 while true; do
@@ -84,8 +70,8 @@ while true; do
     case $main_choice in
         1) setup_fortnite_proton ;;
         2) launch_application ;;
-        6) diagnose_application ;;
-        7) echo "Fermeture du gestionnaire." ; exit 0 ;;
-        *) echo "Option invalide. Veuillez réessayer." ;;
+        3) diagnose_application ;;
+        4) echo "Closing Manager." ; exit 0 ;;
+        *) echo "Invalid Choice. Put a valid number" ;;
     esac
 done
