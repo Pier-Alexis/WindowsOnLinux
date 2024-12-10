ad74d513834f130b395a80045622e793fa1a3efb
Senka107 <93744556+Pier-Alexis@users.noreply.github.com>
2024-12-10 18:56:18 -0500

    Update configure_mime.sh

--- configure_mime.sh
+++ configure_mime.sh
@@ -1,7 +1,8 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<mime-info>
-    <mime-type type="application/x-ms-dos-executable">
-        <comment>MS-DOS Executable</comment>
-        <glob pattern="*.exe"/>
-    </mime-type>
-</mime-info>
+#!/bin/bash
+echo "=== Configuration of MIME association for WindowsOnLinux ==="
+echo "application/x-ms-dos-executable exe" | sudo tee /usr/share/mime/packages/application-x-ms-dos-executable.xml
+echo "application/x-msi msi" | sudo tee /usr/share/mime/packages/application-x-msi.xml
+# Update MIME base
+sudo update-mime-database /usr/share/mime
+sudo update-desktop-database
+echo "MIME associations finished ! WindowsOnLinux is now recommanded for opening .exe and .msi files."
